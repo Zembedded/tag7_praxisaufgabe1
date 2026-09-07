7f3c817c4fff690836db7473df3424a1634ff618
Martin Zietala <martin@zietala.ch>
2026-09-07 09:01:07 +0200

    Fix Test

--- test/test_thermostat/test_thermostat.cpp
+++ test/test_thermostat/test_thermostat.cpp
@@ -34,7 +34,7 @@ class ThermostatTest : public ::testing::Test {
 };
 
 TEST_F(ThermostatTest, StartsIdle) {
-    EXPECT_TRUE(thermostat.isHeating());
+    EXPECT_FALSE(thermostat.isHeating());
 }
 
 TEST_F(ThermostatTest, ReturnsSetpointAndHysteresisFromConstructor) {
